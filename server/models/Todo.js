6efab55dd97984f2882bb9568bb793a5a6f068ae
Adamniels <adamnielsen401@gmail.com>
2025-03-14 17:22:06 +0100

    fixed with the todolist

---
+++ server/models/Todo.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const TodoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: false,
+  },
+  priority: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  // TODO:
+  //  - deadline
+});
+
+module.exports = mongoose.model("Todo", TodoSchema);
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,26 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Todo = require("../models/Todo");
 
-router.get('', (req, res) => {
-const locals = {
+/**
+ * GET
+ * FIRSTPAGE/LOGIN
+ */
+router.get("", (req, res) => {
+  const locals = {
     title: "Master Project",
-    desc: "training for master project"
-}
+    desc: "training for master project",
+  };
 
-    res.render('index', {locals, layout: 'layouts/unauthorized'}); 
+  res.render("index", { locals, layout: "layouts/unauthorized" });
 });
 
-router.get('/about', (req, res) => {
-    res.render('about');
-})
+/**
+ * GET
+ * HOME
+ */
+router.get("/homepage", async (req, res) => {
+  const locals = {
+    title: "Master Project",
+    desc: "training for master project",
+  };
 
-router.get('/homepage', (req, res) => {
-    const locals = {
-        title: "Master Project",
-        desc: "training for master project"
-    }
-    
-        res.render('homepage', {locals});
-    });
+  try {
+    const data = await Todo.find();
+    res.render("homepage", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * GET
+ * ABOUT
+ */
+router.get("/about", (req, res) => {
+  res.render("about");
+});
 
 module.exports = router;
+
+function insertTodosOnce() {
+  Todo.insertMany([
+    {
+      title: "Clean out the garage",
+      body: "throw away garbage and vacuum",
+      priority: 1,
+    },
+    {
+      title: "Cook",
+      body: "Make carbonara",
+      priority: 2,
+    },
+  ]);
+}
+
+//insertTodosOnce();
